a51e4f0e68cde8619b100035022d16ba3cd1d09c
ovavellan <oscaravellan2000@gmail.com>
2023-12-19 17:51:13 -0500

    estilos para formulario
    estilos para formulario

--- src/app/home/pages/formulario-registro/formulario-registro.component.ts
+++ src/app/home/pages/formulario-registro/formulario-registro.component.ts
@@ -28,7 +28,7 @@ export class FormularioRegistroComponent implements OnInit{
       modelo: new FormControl('', [Validators.required]),
       date: new FormControl('', [Validators.required]),
       horaIngreso: new FormControl('', [Validators.required, Validators.pattern("^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$")]),
-      mecaninco: new FormControl('', [Validators.required]),
+      empleado: new FormControl('', [Validators.required]),
       tip_lavado: new FormControl('', Validators.required)
     })
   }
