a2e42b2a6b618b11e1478fea02dc9a0109b9abbf
llmosquera <lmmosquera@espe.edu.ec>
2024-03-12 18:53:53 -0500

    Pruebas Unitarias Cargar Archivo
    Pruebas unitarias de cargar archivo

--- src/app/home/components/cargar-archivo/cargar-archivo.component.spec.ts
+++ src/app/home/components/cargar-archivo/cargar-archivo.component.spec.ts
@@ -1,21 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { CargarArchivoComponent } from './cargar-archivo.component';
 
 describe('CargarArchivoComponent', () => {
   let component: CargarArchivoComponent;
   let fixture: ComponentFixture<CargarArchivoComponent>;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CargarArchivoComponent]
-    });
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(CargarArchivoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit the selected file', () => {
+    const file = new File(['sample content'], 'sample.txt');
+    const event = { target: { files: [file] } };
+    spyOn(component.archivoCargado, 'emit');
+    
+    component.onFileSelected(event);
+
+    expect(component.archivoCargado.emit).toHaveBeenCalledWith(file);
   });
 });
