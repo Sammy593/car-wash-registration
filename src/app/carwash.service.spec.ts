16c593d1339af79280339ff1295cd0cdbdfec3c6
Jhanira-Elizabeth <56014432+Jhanira-Elizabeth@users.noreply.github.com>
2024-03-11 21:43:48 -0500

    Test Layout
    Se testea la clase layout

--- src/app/carwash.service.spec.ts
+++ src/app/carwash.service.spec.ts
@@ -1,16 +1,87 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CarwashService } from './carwash.service';
 
 describe('CarwashService', () => {
   let service: CarwashService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CarwashService]
+    });
     service = TestBed.inject(CarwashService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should insert a new record', () => {
+    const testData = { id: '1', name: 'John' };
+
+    service.insetarRegistro(testData).then(response => {
+      expect(response).toEqual(testData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3500/solicitud/');
+    expect(req.request.method).toBe('POST');
+    req.flush(testData);
+  });
+
+  it('should get pending state', () => {
+    const testData = { id: '1', status: 'pending' };
+
+    service.getEstadoPendiente().then(response => {
+      expect(response).toEqual(testData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3500/solicitud/getTabla1');
+    expect(req.request.method).toBe('GET');
+    req.flush(testData);
+  });
+
+  it('should accept a request', () => {
+    const testData = { id: '1', status: 'accepted' };
+
+    service.aceptarSolicitud('1').then(response => {
+      expect(response).toEqual(testData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3500/solicitud/aceptarRegistro');
+    expect(req.request.method).toBe('POST');
+    req.flush(testData);
+  });
+
+  it('should reject a request', () => {
+    const testData = { id: '1', status: 'rejected' };
+
+    service.rechazarSolicitud('1').then(response => {
+      expect(response).toEqual(testData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3500/solicitud/rechazarRegistro');
+    expect(req.request.method).toBe('POST');
+    req.flush(testData);
+  });
+
+  it('should update a request', () => {
+    const testData = { id: '1', status: 'updated' };
+    const updatedData = { name: 'Updated Name' };
+
+    service.actualizarSolicitud('1', updatedData).then(response => {
+      expect(response).toEqual(testData);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3500/solicitud/actualizarRegistro');
+    expect(req.request.method).toBe('POST');
+    req.flush(testData);
+  });
+
 });
--- src/app/home/components/layout/layout.component.spec.ts
+++ src/app/home/components/layout/layout.component.spec.ts
@@ -1,21 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LayoutComponent } from './layout.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LayoutComponent2 } from './layout.component';
 
 describe('LayoutComponent', () => {
-  let component: LayoutComponent;
-  let fixture: ComponentFixture<LayoutComponent>;
+  let component: LayoutComponent2;
+  let fixture: ComponentFixture<LayoutComponent2>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LayoutComponent2],
+      imports: [RouterTestingModule]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LayoutComponent]
-    });
-    fixture = TestBed.createComponent(LayoutComponent);
+    fixture = TestBed.createComponent(LayoutComponent2);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render router-outlet', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const routerOutlet = element.querySelector('router-outlet');
+    expect(routerOutlet).toBeTruthy();
   });
 });
