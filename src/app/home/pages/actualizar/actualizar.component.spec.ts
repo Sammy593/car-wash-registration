d7cebadb34be96cb965052f53d32b8621e3adcac
llmosquera <lmmosquera@espe.edu.ec>
2024-03-12 22:37:36 -0500

    corregido

--- src/app/home/pages/actualizar/actualizar.component.spec.ts
+++ src/app/home/pages/actualizar/actualizar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { ActualizarComponent } from './actualizar.component';
 
-fdescribe('ActualizarComponent', () => {
+describe('ActualizarComponent', () => {
   let component: ActualizarComponent;
   let fixture: ComponentFixture<ActualizarComponent>;
 
