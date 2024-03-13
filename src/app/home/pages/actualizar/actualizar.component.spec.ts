27b787a3f562ffeef8eb53598c20197d9485fc57
llmosquera <lmmosquera@espe.edu.ec>
2024-03-12 23:24:32 -0500

    arreglando

--- src/app/home/pages/actualizar/actualizar.component.spec.ts
+++ src/app/home/pages/actualizar/actualizar.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 import { ActualizarComponent } from './actualizar.component';
 
 describe('ActualizarComponent', () => {
@@ -9,7 +11,7 @@ describe('ActualizarComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ActualizarComponent],
-      imports: [HttpClientModule]
+      imports: [HttpClientModule, RouterTestingModule, FormsModule],
     });
     fixture = TestBed.createComponent(ActualizarComponent);
     component = fixture.componentInstance;
@@ -19,4 +21,40 @@ describe('ActualizarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should disable button when form is invalid', () => {
+    component.nombre_cliente = ''; // Invalid form state
+    component.cedula = '1234567890';
+    component.direccion = '123 Street';
+    component.email = 'test@example.com';
+    component.marca_vehiculo = 'Toyota';
+    component.modelo_vehiculo = 'Corolla';
+    component.fecha_ingreso = '2024-03-12';
+    component.hora_ingreso = '12:00';
+    component.tipo_lavado = 'Económico';
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeTruthy();
+  });
+
+  it('should enable button when form is valid', () => {
+    component.nombre_cliente = 'John Doe';
+    component.cedula = '1234567890';
+    component.direccion = '123 Street';
+    component.email = 'test@example.com';
+    component.marca_vehiculo = 'Toyota';
+    component.modelo_vehiculo = 'Corolla';
+    component.fecha_ingreso = '2024-03-12';
+    component.hora_ingreso = '12:00';
+    component.tipo_lavado = 'Económico';
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeFalsy();
+  });
+
+  // Add more test cases as needed to cover all functionalities
 });
--- src/app/home/pages/formulario-registro/formulario-registro.component.spec.ts
+++ src/app/home/pages/formulario-registro/formulario-registro.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormularioRegistroComponent } from './formulario-registro.component';
+import {NgxCaptchaModule} from "ngx-captcha";
 
-describe('FormularioRegistroComponent', () => {
+fdescribe('FormularioRegistroComponent', () => {
   let component: FormularioRegistroComponent;
   let fixture: ComponentFixture<FormularioRegistroComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FormularioRegistroComponent]
+      declarations: [FormularioRegistroComponent],
+      imports: [HttpClientModule, RouterTestingModule, FormsModule, ReactiveFormsModule, NgxCaptchaModule], 
     });
     fixture = TestBed.createComponent(FormularioRegistroComponent);
     component = fixture.componentInstance;
