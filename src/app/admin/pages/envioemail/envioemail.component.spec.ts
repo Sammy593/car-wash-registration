4b7f587fd13b3ad271af1b1f091ad0649b0960e3
Jhanira-Elizabeth <56014432+Jhanira-Elizabeth@users.noreply.github.com>
2024-03-13 01:13:13 -0500

    Revert "Merge branch 'main' of https://github.com/Sammy593/car-wash-registration"
    This reverts commit 8c58c3caba7ca52fa5c976547d6d0a2439a744c1, reversing
    changes made to eff4273416be61b8584eaab8f37859f39aa779cb.

--- src/app/admin/pages/envioemail/envioemail.component.spec.ts
+++ src/app/admin/pages/envioemail/envioemail.component.spec.ts
@@ -1,21 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { EnvioemailComponent } from './envioemail.component';
-import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
 
+import { EnvioemailComponent } from './envioemail.component';
 
 describe('EnvioemailComponent', () => {
   let component: EnvioemailComponent;
   let fixture: ComponentFixture<EnvioemailComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EnvioemailComponent ],
-      imports: [HttpClientModule]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EnvioemailComponent]
+    });
     fixture = TestBed.createComponent(EnvioemailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,30 +18,4 @@ describe('EnvioemailComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should redirect when redirectToMostrarDetalle is called', () => {
-    const spy = spyOn(component, 'redirectToMostrarDetalle');
-    const id = 123; // Assuming ID is a number
-    component.redirectToMostrarDetalle(id);
-    expect(spy).toHaveBeenCalledWith(id);
-  });
-
-  it('should call impagoSolicitud when impagoSolicitud is called', () => {
-    const spy = spyOn(component, 'impagoSolicitud');
-    const id = 456; // Assuming ID is a number
-    component.impagoSolicitud(id.toString()); // Convert id to string
-    expect(spy).toHaveBeenCalledWith(id.toString());
-});
-
-it('should call rechazarSolicitud when rechazarSolicitud is called', () => {
-    const spy = spyOn(component, 'rechazarSolicitud');
-    const id = 789; // Assuming ID is a number
-    component.rechazarSolicitud(id.toString()); // Convert id to string
-    expect(spy).toHaveBeenCalledWith(id.toString());
-});
-
-
-
-
-  // You can add more test cases based on your component's logic
 });
